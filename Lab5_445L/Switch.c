f106c11a4b134550b36f48aadb05e019327bdb13
danieldiamont <daniel.diamont@utexas.edu>
2018-02-19 16:46:03 -0600

    update

--- Lab5_445L/Switch.c
+++ Lab5_445L/Switch.c
@@ -33,6 +33,11 @@ long StartCritical (void);    // previous I bit, disable interrupts
 void EndCritical(long sr);    // restore I bit to previous value
 void WaitForInterrupt(void);  // low power mode
 
+#define PF0             (*((volatile uint32_t *)0x40025004))
+#define LOCK_REG				(*((volatile uint32_t *)(GPIO_O_LOCK + GPIO_PORTF_BASE)))
+#define CR_REG					(*((volatile uint32_t *)(GPIO_O_CR + GPIO_PORTF_BASE)))
+
+
 volatile static unsigned long Touch;     // true on touch
 volatile static unsigned long Release;   // true on release
 volatile static unsigned long Last;      // previous
@@ -74,6 +79,21 @@ void Switch_Init(void(*touchtask)(void), void(*releasetask)(void)){
   GPIO_PORTF_PUR_R |= 0x10;     //     enable weak pull-up on PF4
   GPIO_PORTF_IS_R &= ~0x10;     // (d) PF4 is edge-sensitive
   GPIO_PORTF_IBE_R |= 0x10;     //     PF4 is both edges
+
+  LOCK_REG = GPIO_LOCK_KEY;	//unlock pf0
+  CR_REG = 0x000000FF;		//commit the unlock
+  GPIO_PORTF_DIR_R &= ~0x01;    // (c) make PF0 in (built-in button)
+  GPIO_PORTF_AFSEL_R &= ~0x01;  //     disable alt funct on PF0
+  GPIO_PORTF_DEN_R |= 0x01;     //     enable digital I/O on PF0   
+  GPIO_PORTF_PCTL_R &= ~0x0000000F; // configure PF0 as GPIO
+  GPIO_PORTF_AMSEL_R = 0;       //     disable analog functionality on PF
+  GPIO_PORTF_PUR_R |= 0x01;     //     enable weak pull-up on PF0
+  GPIO_PORTF_IS_R &= ~0x01;     // (d) PF0 is edge-sensitive
+  GPIO_PORTF_IBE_R |= 0x01;     //     PF0 is both edges
+
+
+  //PD2, and PD3
+
   GPIOArm();
 
   SYSCTL_RCGCTIMER_R |= 0x01;   // 0) activate TIMER0
@@ -85,8 +105,9 @@ void Switch_Init(void(*touchtask)(void), void(*releasetask)(void)){
  }
 // Interrupt on rising or falling edge of PF4 (CCP0)
 void GPIOPortF_Handler(void){
-  GPIO_PORTF_IM_R &= ~0x10;     // disarm interrupt on PF4 
-  if(Last){    // 0x10 means it was previously released
+
+  GPIO_PORTF_IM_R &= ~0x11;     // disarm interrupt on all of port f 
+  if(Last){    // 0x11 means either PF4 or PF0 was previously released
     Touch = 1;       // touch occurred
     (*TouchTask)();  // execute user task
   }
