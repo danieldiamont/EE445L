7a12dc75c81aedf6d4e42fde9f4b646263b905c0
RobertNoe <robertnoe6626@gmail.com>
2018-02-25 19:41:34 -0600

    thing

--- Lab5_445L/Switch.c
+++ Lab5_445L/Switch.c
@@ -181,7 +181,7 @@ void DelayWait10ms(uint32_t n){
 void GPIOPortF_Handler(void){
 
   GPIO_PORTF_IM_R &= ~0x11;
-	if((GPIO_PORTF_DATA_R>>4)%0x01)
+	if((GPIO_PORTF_DATA_R>>4)%0x01==0)
 	{
 		int start =NVIC_ST_CURRENT_R;
 		//while(NVIC_ST_CURRENT_R-start<30000){}
