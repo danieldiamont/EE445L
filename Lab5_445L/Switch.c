d16735ad5f5497d8bf54a672b4cf121147ae1559
RobertNoe <robertnoe6626@gmail.com>
2018-02-25 19:43:25 -0600

    thing

--- Lab5_445L/Switch.c
+++ Lab5_445L/Switch.c
@@ -186,9 +186,13 @@ void GPIOPortF_Handler(void){
 		int start =NVIC_ST_CURRENT_R;
 		//while(NVIC_ST_CURRENT_R-start<30000){}
 		DelayWait10ms(3);
-			
 		Sound_Play_Song(0,0);
-	}		
+	}
+	else
+	{
+		DelayWait10ms(3);
+	}
+
 	// disarm interrupt on all of port f 
  // if(Last){    // 0x11 means either PF4 or PF0 was previously released
     //Touch = 1;       // //Touch occurred
