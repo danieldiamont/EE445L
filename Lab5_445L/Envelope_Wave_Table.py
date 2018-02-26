f296160979a4d3b1b9507ab4ef4dc9f8bc57d080
danieldiamont <danie.diamont@utexas.edu>
2018-02-26 11:20:49 -0600

    256 entry wave with envelope

--- Lab5_445L/Envelope_Wave_Table.py
+++ Lab5_445L/Envelope_Wave_Table.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 wave = [
   2048*2,2448*2,2832*2,3186*2,3496*2,3751*2,3940*2,4057*2,4095*2,4057*2,3940*2,
@@ -10,4 +11,14 @@ new_wave = []
 for i in range(0,31):
     new_wave.append((int)(wave[i]*math.exp(-i/8)))
 
-print(new_wave)
+Fs = 256
+f = 5
+sample = 256
+x = np.arange(sample)
+y = 2048*np.cos(2*np.pi*f*x/Fs)+2048
+wav = []
+for i in range(0,y.size-1):
+    wav.append(int(y[i]*math.exp(-i/256)))
+
+print(wav)
+print('size: ' + str(y.size))
